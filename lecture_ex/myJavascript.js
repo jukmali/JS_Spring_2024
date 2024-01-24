2ef9d8948323cf081758dd7a81c68335691b3a7f
Jukka Malinen <Jukka.malinen@laurea.fi>
2024-01-24 09:58:00 +0200

    new files

--- lecture_ex/myJavascript.js
+++ lecture_ex/myJavascript.js
@@ -1,6 +1,14 @@
-alert("hello world");
+
 
 function myFunction(a,b){
 
     return a+b;
+}
+
+function myFirstFunction(number){
+    for(var i=0; i < number; i++){
+        console.log(i);
+    }
+
+    return 10;
 }
